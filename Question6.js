ebfbc60840a630e8241bf5736a475c7035958294
reshma325 <138295298+reshma325@users.noreply.github.com>
2023-12-01 19:48:24 +0530

    update 1st Dec

---
+++ Question6.js
@@ -0,0 +1,17 @@
+const obj =  {name: 'John', age: 25, city: 'New York'}
+const key1 = 'name'
+
+if((Object.keys(obj).find(key => key.includes(key1))) == key1){
+    console.log('True')
+} else {
+    console.log('False')
+}
+
+// const d3Array = [[[1,2,3,4,5],[6,7,8,9,0]],[11,22,33,44,55]]
+
+// console.log(d3Array[1])
+
+// for(int i =0; i < ){
+
+
+// }
---
+++ new.js
@@ -0,0 +1,55 @@
+let array = [
+    {
+        id: 10689,
+        sport: 'Tennis',
+        players: [
+            {
+                id: 22,
+                name: 'Rafa Nadal',
+                country: 'Spain',
+                odds: {
+                    bookie_1: 1.60,
+                    bookie_2: 1.61,
+                    bookie_3: 1.62
+                }
+            },
+            {
+                id: 23,
+                name: 'Roger Federer',
+                country: 'Spain',
+                odds: {
+                    bookie_1: 2.30,
+                    bookie_2: 2.31,
+                    bookie_3: 2.32
+                }
+            }
+        ]
+    },
+    {
+        id: 12389,
+        sport: 'Tennis',
+        players: [
+            {
+                id: 45,
+                name: 'Fernando Verdasco',
+                country: 'Spain',
+                odds: {
+                    bookie_1: 3.20,
+                    bookie_2: 3.21,
+                    bookie_3: 3.22
+                }
+            },
+            {
+                id: 65,
+                name: 'Andy Murray',
+                country: 'Spain',
+                odds: {
+                    bookie_1: 1.20,
+                    bookie_2: 1.21,
+                    bookie_3: 1.22
+                }
+            }
+        ]
+    }
+];
+console.log(array[1])
